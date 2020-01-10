7f18ea76179c48e3a364aed093704636caf92f4e
Danny Calise <dannyjcalise@gmail.com>
2020-01-09 20:12:16 -0500

    added controllers

---
+++ src/components/card.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+
+class Card extends Component {
+
+    render() {
+        return (
+            <Card className="text-center">
+                <Card.Header>Featured</Card.Header>
+                <Card.Body>
+                    <Card.Title>Special title treatment</Card.Title>
+                    <Card.Text>
+                        With supporting text below as a natural lead-in to additional content.
+                    </Card.Text>
+                    <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+                <Card.Footer className="text-muted">2 days ago</Card.Footer>
+            </Card>
+        )
+    }
+}
+
+export default Card
