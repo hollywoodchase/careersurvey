3c87f36c93fa3e884b3dd8b9425612afbed85609
Danny Calise <dannyjcalise@gmail.com>
2020-01-16 19:07:26 -0500

    added jobs api route

--- server/database/models/job.js
+++ server/database/models/job.js
@@ -2,15 +2,31 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const jobSchema = new Schema({
-  title: { type: String, required: true },
-  category: { type: String, required: true },
-  salary: String,
-  hours: String,
-  education: String,
-  people: Boolean
+  rank: { type: String, required: true },
+  description: { type: String, required: true },
+  image: { type: String, required: true },
+  projectedJobs: { type: String, required: true },
+  medianSalary: { type: String, required: true },
+  hourlyWage: { type: String, required: true },
+  rent: { type: String, required: true },
+  link: { type: String, required: true },
+  questionShift: { type: String, required: true },
+  questionIncome: { type: String, required: true },
+  questionTech: { type: Boolean, required: true },
+  questionHealth: { type: String, required: true },
+  questionEducation: { type: String, required: true },
+  questionPeople: { type: Boolean, required: true },
+  questionSubject: { type: String, required: true },
+  questionBuild: { type: String, required: true },
+  questionPriority: { type: String, required: true },
+  questionWhere: { type: String, required: true },
+  questionEnvironment: { type: Boolean, required: true },
+  questionHands: { type: Boolean, required: true }
 });
 
 const Job = mongoose.model("Job", jobSchema);
 
 module.exports = Job;
+
+
 
