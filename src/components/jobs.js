683a96898e23e823e485e4fe7608b4ae0167bbe1
Kevin Perez <kevinperez996@outlook.com>
2020-01-23 19:49:04 -0500

    added Survey Buttons if survey has not been taken

--- src/components/jobs.js
+++ src/components/jobs.js
@@ -16,26 +16,34 @@ class Jobs extends Component {
     getJobs = () => {
         axios.get('/api/jobs')
             .then(res => {
-                for (let i = 0; i < res.data.length; i++) {
-                    console.log("FOOL " + JSON.stringify(res.data[0]))
-                    jobInfo.push({
-                        id: res.data[i]._id,
-                        title: res.data[i].title,
-                        description: res.data[i].description,
-                        salary: res.data[i].medianSalary,
-                        jobsAvailable: res.data[i].projectedJobs,
-                        link: res.data[i].link
-                    })
+                if (res.data.length < 1) {
+                    document.getElementById('cardTitle').innerHTML = 'Extra stuff';    
                 }
-                this.setState({ info: jobInfo })         
+                else {
+                    document.getElementById("startSurvey").style.visibility = "hidden"
+                    document.getElementById('cardTitle').innerHTML = 'Suggested Jobs'
+                    for (let i = 0; i < res.data.length; i++) {
+                        console.log("FOOL " + JSON.stringify(res.data[0]))
+                        jobInfo.push({
+                            id: res.data[i]._id,
+                            title: res.data[i].title,
+                            description: res.data[i].description,
+                            salary: res.data[i].medianSalary,
+                            jobsAvailable: res.data[i].projectedJobs,
+                            link: res.data[i].link
+                        })
+                    }
+                    this.setState({ info: jobInfo })
+                }
             });
     }
 
     render() {
         return (
+
             <div className="card">
                 <div className="card-header">
-                    <h3>Suggested Jobs</h3>
+                    <h3 id="cardTitle"></h3>
                 </div>
                 <div className="card-body">
                     {this.state.info.map((info, index) => (
@@ -44,12 +52,13 @@ class Jobs extends Component {
                             <p className="card-description">Description: {JSON.stringify(info.description)}</p>
                             <p className="card-salary">Median Salary: {JSON.stringify(info.salary)}</p>
                             <p className="card-jobsAvailable">Available Jobs: {JSON.stringify(info.jobsAvailable)}</p>
-                            <a href={JSON.stringify(info.link)} className="infobtn seemoreButton btn btn-info"><h6>See More</h6></a>
+                            <a href={JSON.stringify(info.link)} target="_blank" className="infobtn seemoreButton btn btn-info"><h6>See More</h6></a>
                             <a href="#" className="infobtn saveButton btn btn-secondary"><h6>Save</h6></a>
                             <a href="#" className="infobtn deleteButton btn btn-danger"><h6>Delete</h6></a>
                         </div>
                     ))}
                 </div>
+                <div id= "surveyBtnDiv"><a href="/survey" id="startSurvey" className="btn btn-secondary"><h4>Start Survey</h4></a></div>
             </div>
 
         )
